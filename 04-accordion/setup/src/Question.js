c29692d4e7c25d2fe065291c5641978382fa0fb8
Rithin <rithinkumar13@gmail.com>
2021-06-16 10:31:22 +0530

    Accordian Menu Project done.

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, id, info }) => {
+  const [minimized, setMinimized] = useState(true);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setMinimized(!minimized);
+          }}
+        >
+          {minimized ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      {!minimized && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
